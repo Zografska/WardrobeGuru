4f98de62d7f543231ca00379c4c569648a6b2abe
“Mila <mila@halicea.com>
2023-09-14 17:15:32 +0200

    [FTR] | Add clothing items mock data

--- WardrobeGuru/WardrobeGuru/Pages/Search/SearchPageViewModel.cs
+++ WardrobeGuru/WardrobeGuru/Pages/Search/SearchPageViewModel.cs
@@ -25,22 +25,71 @@ namespace WardrobeGuru.Pages.Search
         {
             new ClothingItem
             {
-                Name = "name1",
-                Description = "test1",
+                Name = "Classic Denim Jacket",
+                Description = "A timeless classic, this denim jacket is perfect for layering and adding a touch of rugged style to any outfit.",
                 Image = new ImageModel(ImagePath, FilePath),
                 Status = "Available"
             },
             new ClothingItem
             {
-                Name = "name2",
-                Description = "test2",
+                Name = "Cozy Knit Sweater",
+                Description = "Stay warm and stylish with this cozy knit sweater. Its soft, textured fabric and relaxed fit make it a must-have for chilly days.",
                 Image = new ImageModel(ImagePath, FilePath),
                 Status = "Available"
             },
             new ClothingItem
             {
-                Name = "name3",
-                Description = "test3",
+                Name = "Floral Maxi Dress",
+                Description = "Embrace the beauty of nature with this elegant floral maxi dress. It features a flattering A-line silhouette and a vibrant floral print.",
+                Image = new ImageModel(ImagePath, FilePath),
+                Status = "Available"
+            },
+            new ClothingItem
+            {
+                Name = "Slim-Fit Tailored Suit",
+                Description = "Elevate your formal attire with this slim-fit tailored suit. It exudes sophistication and is perfect for business meetings or special occasions.",
+                Image = new ImageModel(ImagePath, FilePath),
+                Status = "Available"
+            },
+            new ClothingItem
+            {
+                Name = "Vintage Graphic T-Shirt",
+                Description = "Add a touch of nostalgia to your wardrobe with this vintage graphic t-shirt. Its retro design and soft cotton fabric make it a comfortable and stylish choice.",
+                Image = new ImageModel(ImagePath, FilePath),
+                Status = "Available"
+            },
+            new ClothingItem
+            {
+                Name = "Active Performance Leggings",
+                Description = "Stay active and comfortable in these performance leggings. They're designed to wick away moisture and provide flexibility for your workouts.",
+                Image = new ImageModel(ImagePath, FilePath),
+                Status = "Available"
+            },
+            new ClothingItem
+            {
+                Name = "Leather Moto Jacket",
+                Description = "Make a bold statement with this leather moto jacket. Its edgy design and high-quality leather will turn heads wherever you go.",
+                Image = new ImageModel(ImagePath, FilePath),
+                Status = "Available"
+            },
+            new ClothingItem
+            {
+                Name = "Striped Cotton Polo Shirt",
+                Description = "Look effortlessly stylish in this striped cotton polo shirt. It's a versatile piece that pairs well with jeans or shorts.",
+                Image = new ImageModel(ImagePath, FilePath),
+                Status = "Available"
+            },
+            new ClothingItem
+            {
+                Name = "Bohemian Fringe Vest",
+                Description = "Channel your inner bohemian spirit with this fringe vest. Its free-spirited design adds a touch of whimsy to your look.",
+                Image = new ImageModel(ImagePath, FilePath),
+                Status = "Available"
+            },
+            new ClothingItem
+            {
+                Name = "Cargo Shorts with Pockets",
+                Description = "Stay practical and stylish with these cargo shorts. Multiple pockets provide ample storage while the relaxed fit ensures comfort for your outdoor adventures.",
                 Image = new ImageModel(ImagePath, FilePath),
                 Status = "Available"
             }
