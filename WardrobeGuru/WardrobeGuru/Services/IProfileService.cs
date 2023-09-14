749f437e562f777ecb550a427c7d60f1da2950ff
“Mila <mila@halicea.com>
2023-09-14 18:13:58 +0200

    [FTR] | Add users mock data and get current user functionality

--- WardrobeGuru/WardrobeGuru/Services/IProfileService.cs
+++ WardrobeGuru/WardrobeGuru/Services/IProfileService.cs
@@ -6,6 +6,7 @@ namespace WardrobeGuru.Services
 {
     public interface IProfileService : IServiceBase<User>
     {
+        string CurrentUser { get; set; }
         Task<User> GetCurrentUser();
         Task<User> CreateUser(string userId, string name, string surname, string email, string photoUrl);
         Task<bool> IsUserExistent(string email);
